288588c48920fbefb039899509fee5361d54c0e5
Joshua <josht.yates@btinternet.com>
2020-06-22 22:45:14 +0100

    Add bunting options and skeleton methods

---
+++ lib/bunting-options.ts
@@ -0,0 +1,18 @@
+export interface BuntingOptions {
+    /**
+     * The range of colours to use for the flags, as hex codes.
+    */
+    colourSet: string[],
+    /**
+     * The colour of the rope, as a hex code.
+     */
+    ropeColour: string,
+    /**
+     * Whether the rope and flags should have a drop shadow.
+     */
+    useShadow: boolean,
+    /**
+     * Whether the bunting should be redrawn upon page resize.
+     */
+    autoRedraw: boolean
+}
